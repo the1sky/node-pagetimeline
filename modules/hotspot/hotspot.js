4b41f769ec067e9cca8d270d4e5ef0e852706f3a
nant <dumingtan@gmail.com>
2014-05-23 14:50:49 +0800

    # finish hotspot, top 10

--- modules/hotspot/hotspot.js
+++ modules/hotspot/hotspot.js
@@ -3,51 +3,70 @@
  */
 exports.version = '0.1.0';
 exports.module = function (timeline) {
+    var TOP_K = 10;
+    var count_duration = function (start, finish) {
+        return ( (finish - start) / 1000000).toFixed(2);
+    };
+
+    var getFuncName = function (log) {
+        var tmp = log.split('\t')[0].split('(')[0].split('::');
+        return tmp[tmp.length - 1];
+    };
+
+    var getParameters = function (log) {
+        var paramStr = log.split('\t')[2];
+        if (paramStr) {
+            return paramStr.split(' ');
+        }
+        return null;
+    };
+
+    var getStartTime = function (log) {
+        return log.split('\t')[1];
+    };
+
     var function_pair_map = {
         "willSendRequest": "didFinishLoading"
     }
     var ignore_functions = {'willRemoveDOMNode': 1,
         'willDestroyCachedResource': 1,
         "didReceiveData": 1};
+
     var filterResult = timeline.getResults().getFilterData();
+
+    var function_type = JSON.parse(require("fs").readFileSync(__dirname + "/function_type.json"));
+
     var len = filterResult.length;
     var startTime = len > 0 ? filterResult[0].split('\t')[1] : 0;
     var hotspot = [];
     for (var i = 0; i < len; i++) {
-        var item = filterResult[i];
-        var itemArr = item.split('\t');
-        var funcPart = itemArr[0];
-        var timePart = itemArr[1];
-        var paramsPart = itemArr[2];
+        var log = filterResult[i];
+        var funcName = getFuncName(log);
 
-        var funcName = funcPart.split('(')[0].split('::')
-        funcName = funcName[funcName.length - 1];
-
-        if( ignore_functions[funcName] ){
+        if (ignore_functions[funcName]) {
             continue;
         }
 
         if (funcName.indexOf('will') == 0 || function_pair_map[funcName]) {
             var pair_function_name = '';
 
-            if( function_pair_map[funcName] ){
+            if (function_pair_map[funcName]) {
                 pair_function_name = function_pair_map[funcName];
-            }else{
-               pair_function_name = funcName.replace(/^will$/, 'did');
+            } else {
+                pair_function_name = funcName.replace(/^will$/, 'did');
             }
 
             var pair_index = i + 1;
             var found = false;
 
-            while( pair_index < len ){
-                if( getFuncName( filterResult[pair_index] ) == pair_function_name ){
-                    if( funcName == 'willSendRequest'){
-                        //todo
-                        if( paramsPart[1] == getParameters(filterResult[pair_index]) ){
+            while (pair_index < len) {
+                if (getFuncName(filterResult[pair_index]) == pair_function_name) {
+                    if (funcName == 'willSendRequest') {
+                        if (getParameters(log)[1] == getParameters(filterResult[pair_index])[0]) {
                             found = true;
                             break;
                         }
-                    }else{
+                    } else {
                         found = true;
                         break;
                     }
@@ -55,50 +74,46 @@ exports.module = function (timeline) {
                 pair_index += 1;
             }
 
-            if( found ){
-                var pair_log = filterResult[pair_index]
-                duration = getStartTime( pair_log ) - getStartTime( item ) / 1000000
+            if (found) {
+                var pair_log = filterResult[pair_index];
+                var duration = ( getStartTime(pair_log) - getStartTime(log) ) / 1000000;
                 var log_type = 'other'
-                if function_name in function_type:
+                if (function_type[funcName]) {
                     log_type = function_type[funcName]["type"]
+                }
 
-                if ( (duration - 0.009) > 0.00001 ){
+                if ((duration - 0.009) > 0.00001) {
+                    var extra = [];
+                    var logParams = getParameters(log);
+                    var pairLogParams = getParameters(pair_log);
+                    if (logParams) {
+                        extra = extra.concat(logParams);
+                    }
+                    if (pairLogParams) {
+                        extra = extra.concat(pairLogParams);
+                    }
+
                     hotspot.push({
-                        "name": log['functionName'].replace('will', ''),
+                        "name": getFuncName(log).replace('will', ''),
                         "type": log_type,
                         "duration": duration,
-                        "extra": log['parameters'] + pair_log['parameters']
+                        "extra": extra.join(' ')
                     });
                 }
             }
         }
     }
 
+    hotspot.sort(function (x, y) {
+        return y['duration'] - x['duration'];
+    });
+    len = hotspot.length < TOP_K ? hotspot.length : TOP_K;
+    hotspot = hotspot.slice(0, len);
 
-    hotspot = sorted(hotspot, key = lambda x:x['duration'], reverse=True)[:TOP_K]
+    timeline.setMetric('hotspot', len);
 
-    for item in hotspot:
-    item['duration'] = "{0:.2f}".format( item['duration'] );
-
-    var count_duration = function (start, finish) {
-        return ( (finish - start) / 1000000).toFixed(2);
-    };
-
-    var getFuncName = function(log){
-        return log.split('\t')[0].split('(')[0].split('::');
-    };
-
-    var getParameters = function(log){
-
-    };
-
-    var getStartTime = function(log){
-        return log.split('\t')[1] : 0;
+    for (i = 0; i < len; i++) {
+        hotspot[i]['duration'] = hotspot[i]['duration'].toFixed(2);
+        timeline.addOffender('hotspot', JSON.stringify(hotspot[i]));
     }
-
-    timeline.setMetric('hotspot', 3);
-
-    timeline.addOffender('hotspot', 'http://www.baidu.com');
-    timeline.addOffender('hotspot', 'http://www.baidu.com/logo.gif');
-    timeline.addOffender('hotspot', 'http://www.baidu.com/test.js');
 }
